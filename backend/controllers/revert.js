29bad9f86e9c19a9bec92ad2f280453031e79826
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-07-13 15:31:21 +0530

    added the file specific functionality

--- backend/controllers/revert.js
+++ backend/controllers/revert.js
@@ -1,4 +1,28 @@
-async function revertRepo() {
-    console.log("Reverting changes in the repository to a previous state based on a commit ID");   
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+const readdir = promisify(fs.readdir); // Promisify the readdir function for asynchronous file reading
+const copyFile = promisify(fs.copyFile); // Promisify the copyFile function for asynchronous file copying
+
+async function revertRepo(commitID) {
+    const repoPath = path.resolve(process.cwd(), '.CodeVault');
+    const commitsPath = path.join(repoPath, 'commits');
+    try {
+        const commitDir = path.join(commitsPath, commitID);
+        const files = await readdir(commitDir); // Read the files in the commit directory
+        const parentDir = path.resolve(repoPath, '..'); // Get the parent directory of the commit directory
+        for (const file of files) {
+            await copyFile(
+                path.join(commitDir, file), // Source file path in the commit directory
+                path.join(parentDir, file) // Destination file path in the parent directory
+            );
+        }
+        console.log(`Reverted to commit ${commitID} successfully.`);
+    } catch (error) {
+        console.error("Error in reverting:", error);
+
+    }
+
 }
 module.exports = { revertRepo };
