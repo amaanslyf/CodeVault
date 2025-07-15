3b3cb7d519b573489a7513ebee14e2fd9f0e09a4
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-07-15 18:15:45 +0530

    incorrect routes fixed

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -7,8 +7,8 @@ const userController = require('../controllers/userController');
 userRouter.get('/allUsers', userController.getAllUsers); 
 userRouter.post('/signup', userController.signup); 
 userRouter.post('/login', userController.login); 
-userRouter.get('/userProfile', userController.getUserProfile); 
-userRouter.put('/updateProfile', userController.updateUserProfile); 
-userRouter.delete('/deleteProfile', userController.deleteUserProfile); 
+userRouter.get('/userProfile/:id', userController.getUserProfile); 
+userRouter.put('/updateProfile/:id', userController.updateUserProfile); 
+userRouter.delete('/deleteProfile/:id', userController.deleteUserProfile); 
 
 module.exports = userRouter; 
